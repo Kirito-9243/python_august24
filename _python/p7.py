b1b6ace4b109c8d8b07146fbd450ab30cf7afe55
Kirito <154519852+Kirito-9243@users.noreply.github.com>
2024-08-20 22:23:34 +0530

    Update p7.py

--- _python/p7.py
+++ _python/p7.py
@@ -1,6 +1,12 @@
 # Problem statement: Program to check if the number is Perfect square
- 
+
+import math as mt
 pre_num = int(input("Enter any number:"))
+pre_sq = mt.sqrt(pre_num)
+if pre_num == (pre_sq)**2:
+ print(pre_num,"is a Prefect square!!")
+
+# Prefect number::
 divisors = []
 for i in range(1,pre_num):
     if pre_num % i == 0:
