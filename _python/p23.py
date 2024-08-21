5c8556c3cdd2920c7d3afea82ae19bdac9d15d77
Kirito <154519852+Kirito-9243@users.noreply.github.com>
2024-08-21 20:15:06 +0530

    Update p23.py

--- _python/p23.py
+++ _python/p23.py
@@ -3,11 +3,10 @@
 num_lines = int(input("Enter the number of lines:"))
 for i in range(1,num_lines+1):
     for j in range(1,num_lines+1):
-        if j == 1 or j == num_lines or i == 1 or i ==num_lines:
+        if j == 0 or j == num_lines - 1 or i == 0 or i == num_lines - 1:
             print("*",end=" ")
-        elif j == i or j == num_lines+1:
-            print("*",end=" ") 
+        elif j == i or j == num_lines - i - 1:
+            print("*",end=" ")
         else:   
             print(" ",end=" ")
-    
     print()
